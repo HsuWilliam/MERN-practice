136643df2ce38a9549de316229ebb9be2caeeb64
William Hsu <hsu870422@gmail.com>
2021-07-12 01:24:42 +0000

    add frontend test with coverage rate and ignore files config

--- frontend/src/test/AddTodo.test.tsx
+++ frontend/src/test/AddTodo.test.tsx
@@ -4,17 +4,17 @@ import AddTodo from '../components/AddTodo'
 
 // TODO: Add some simple test cases, e.g. checking components are rendered successfully
 test('should show the field name and button', () => {
-    render(<AddTodo saveTodo={() => { }} />)
-    const nameText = screen.getByText("Name")
-    expect(nameText).toBeInTheDocument()
-    const nameInput = screen.getByLabelText("Name")
-    expect(nameInput).toHaveAttribute("required")
-  
-    const descText = screen.getByText("Description")
-    expect(descText).toBeInTheDocument()
-    const descInput = screen.getByLabelText("Description")
-    expect(descInput).toHaveAttribute("required")
-  
-    const addButton = screen.getByText("Add Todo")
-    expect(addButton).toHaveAttribute("disabled")
-  })
+  render(<AddTodo saveTodo={() => { }} />)
+  const nameText = screen.getByText("Name")
+  expect(nameText).toBeInTheDocument()
+  const nameInput = screen.getByLabelText("Name")
+  expect(nameInput).toHaveAttribute("required")
+
+  const descText = screen.getByText("Description")
+  expect(descText).toBeInTheDocument()
+  const descInput = screen.getByLabelText("Description")
+  expect(descInput).toHaveAttribute("required")
+
+  const addButton = screen.getByText("Add Todo")
+  expect(addButton).toHaveAttribute("disabled")
+})
